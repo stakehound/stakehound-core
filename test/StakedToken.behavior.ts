97d3f27e9e8a5670b1318beb3c5b758b82475ec9
Edgars Nemše <edgars.nemse@gmail.com>
2020-08-18 20:52:54 +0300

    Increase test coverage

--- test/StakedToken.behavior.ts
+++ test/StakedToken.behavior.ts
@@ -9,93 +9,207 @@ export function shouldBehaveLikeStakedToken(_signers: Signer[], decimalsMultipli
     return ethers.BigNumber.from(amount).mul(decimalsMultiplier)
   }
 
+  describe('Initialization', async function () {
+    it("should be set up properly", async function () {
+      const stakedToken: StakedToken = this.stakedToken
+      const owner = await _signers[0].getAddress();
 
-  it("should be set up properly", async function () {
-    expect(await this.stakedToken.name()).to.equal(this.name);
-    expect(await this.stakedToken.symbol()).to.equal(this.symbol);
-    expect(await this.stakedToken.decimals()).to.equal(this.decimals);
-    expect(await this.stakedToken.totalSupply()).to.equal(this.initialSupply);
-    expect(await this.stakedToken.balanceOf(_signers[0].getAddress())).to.equal(this.initialSupply);
+      expect(await stakedToken.name()).to.equal(this.name);
+      expect(await stakedToken.symbol()).to.equal(this.symbol);
+      expect(await stakedToken.decimals()).to.equal(this.decimals);
+      expect(await stakedToken.totalSupply()).to.equal(this.initialSupply);
+      expect(await stakedToken.balanceOf(owner)).to.equal(this.initialSupply);
+      expect(await stakedToken.owner()).to.equal(owner);
+      expect(await stakedToken.supplyController()).to.equal(owner);
+    });
+
+    it("should reject ETH transfers", async function () {
+      await expect(_signers[1].sendTransaction({to: this.stakedToken.address, value: 1})).to.be.reverted;
+    });
   });
 
-  it("should reject ETH transfers", async function () {
-    await expect(_signers[1].sendTransaction({to: this.stakedToken.address, value: 1})).to.be.reverted;
+  describe("setSupplyController", async function () {
+    it("should update supply controller", async function () {
+      const stakedToken: StakedToken = this.stakedToken;
+      const userAddress = await _signers[1].getAddress();
+
+      await expect(stakedToken.setSupplyController(userAddress))
+        .to.emit(stakedToken, 'LogSupplyControllerUpdated')
+        .withArgs(userAddress);
+
+      const stakedTokenAsUser = stakedToken.connect(_signers[1]);
+      const supplyIncrease = toTokenAmount(10);
+      await expect(stakedTokenAsUser.distributeTokens(supplyIncrease))
+        .to.emit(stakedTokenAsUser, 'LogTokenDistribution')
+        .withArgs(this.initialSupply.add(supplyIncrease));
+    });
+
+
+    it("should not be callable by others", async function () {
+      const userAddress = await _signers[1].getAddress();
+      const stakedTokenAsUser = this.stakedToken.connect(_signers[1]);
+      await expect(stakedTokenAsUser.setSupplyController(userAddress))
+        .to.be.reverted;
+    });
   });
 
-  it("should transfer tokens", async function () {
-    const stakedToken: StakedToken = this.stakedToken
 
-    const recipient = await _signers[1].getAddress();
 
-    const transferAmount = toTokenAmount(10);
-    await expect(stakedToken.transfer(recipient, transferAmount))
-      .to.emit(stakedToken, 'Transfer')
-      .withArgs(await _signers[0].getAddress(), recipient, transferAmount);
 
-    expect(await stakedToken.balanceOf(await _signers[0].getAddress())).to.equal(this.initialSupply.sub(transferAmount));
-    expect(await stakedToken.balanceOf(recipient)).to.equal(transferAmount);
-    expect(await stakedToken.totalSupply()).to.equal(this.initialSupply);
+  describe("Transfers", async function () {
+    it("should transfer tokens", async function () {
+      const stakedToken: StakedToken = this.stakedToken
+      const recipient = await _signers[1].getAddress();
+
+      const transferAmount = toTokenAmount(10);
+      await expect(stakedToken.transfer(recipient, transferAmount))
+        .to.emit(stakedToken, 'Transfer')
+        .withArgs(await _signers[0].getAddress(), recipient, transferAmount);
+
+      expect(await stakedToken.balanceOf(await _signers[0].getAddress())).to.equal(this.initialSupply.sub(transferAmount));
+      expect(await stakedToken.balanceOf(recipient)).to.equal(transferAmount);
+      expect(await stakedToken.totalSupply()).to.equal(this.initialSupply);
+    });
+
+
+    it("should fail to transfer too many tokens", async function () {
+      const stakedToken: StakedToken = this.stakedToken
+      const recipient = await _signers[1].getAddress();
+
+      const transferAmount = toTokenAmount(10000);
+      await expect(stakedToken.transfer(recipient, transferAmount))
+        .to.be.reverted;
+
+      expect(await stakedToken.balanceOf(await _signers[0].getAddress())).to.equal(this.initialSupply);
+      expect(await stakedToken.balanceOf(recipient)).to.equal(0);
+      expect(await stakedToken.totalSupply()).to.equal(this.initialSupply);
+    });
   });
 
-   it("should mint new tokens", async function () {
-    const stakedToken: StakedToken = this.stakedToken;
 
-    const mintAmount = toTokenAmount(1);
-    const recipient = await _signers[1].getAddress();
-    await expect(stakedToken.mint(recipient, mintAmount))
-      .to.emit(stakedToken, 'Transfer')
-      .withArgs(ethers.constants.AddressZero, recipient, mintAmount);
+  describe("Minting", async function () {
+    it("should mint new tokens", async function () {
+      const stakedToken: StakedToken = this.stakedToken;
 
-    expect(await stakedToken.balanceOf(recipient)).to.equal(mintAmount)
-  });
-  // Should fail to mint by not supply controller
+      const amount = toTokenAmount(1);
+      const recipient = await _signers[1].getAddress();
+      await expect(stakedToken.mint(recipient, amount))
+        .to.emit(stakedToken, 'Transfer')
+        .withArgs(ethers.constants.AddressZero, recipient, amount);
 
+      expect(await stakedToken.balanceOf(recipient)).to.equal(amount)
+    });
 
-  it("should burn tokens", async function () {
-    const stakedToken: StakedToken = this.stakedToken;
-
-    const burnAmount = toTokenAmount(1);
-    const admin = await _signers[0].getAddress();
-
-    await expect(stakedToken.burn(burnAmount))
-      .to.emit(stakedToken, 'Transfer')
-      .withArgs(admin, ethers.constants.AddressZero, burnAmount);
-
-    expect(await stakedToken.balanceOf(admin)).to.equal(this.initialSupply.sub(burnAmount));
+    it("should not be callable by others", async function () {
+      const userAddress = await _signers[1].getAddress();
+      const stakedTokenAsUser = this.stakedToken.connect(_signers[1]);
+      const amount = toTokenAmount(1);
+      await expect(stakedTokenAsUser.mint(userAddress, amount))
+        .to.be.reverted;
+    });
   });
 
-  // Should fail to burn more than in account
-  // Should fail to burn by not supply controller
+  describe("Burning", async function () {
+    it("should burn tokens", async function () {
+      const stakedToken: StakedToken = this.stakedToken;
 
+      const amount = toTokenAmount(1);
+      const admin = await _signers[0].getAddress();
 
-  it("should update supply controller", async function () {
-    const stakedToken: StakedToken = this.stakedToken
+      await expect(stakedToken.burn(amount))
+        .to.emit(stakedToken, 'Transfer')
+        .withArgs(admin, ethers.constants.AddressZero, amount);
+
+      expect(await stakedToken.balanceOf(admin)).to.equal(this.initialSupply.sub(amount));
+    });
+
+    // Should fail to burn more than in account
+
+    it("should not be callable by others", async function () {
+      const stakedTokenAsUser = this.stakedToken.connect(_signers[1]);
+      const amount = toTokenAmount(1);
+      await expect(stakedTokenAsUser.burn(amount))
+        .to.be.reverted;
+    });
   });
 
-  it("should distribute rewards", async function () {
-    const stakedToken: StakedToken = this.stakedToken
 
-    // Set up other account
-    const mintAmount = toTokenAmount(2000);
-    const recipient = await _signers[1].getAddress();
-    await stakedToken.mint(recipient, mintAmount);
+  describe("Reward distribution", async function () {
+    it("should distribute rewards", async function () {
+      const stakedToken: StakedToken = this.stakedToken
 
-    const supplyIncrease = toTokenAmount(10);
-    await expect(stakedToken.distributeTokens(supplyIncrease))
-      .to.emit(stakedToken, 'LogTokenDistribution')
-      .withArgs(toTokenAmount(3010));
+      // Set up other account
+      const mintAmount = toTokenAmount(2000);
+      const recipient = await _signers[1].getAddress();
+      await stakedToken.mint(recipient, mintAmount);
 
-    expect(await stakedToken.balanceOf(await _signers[0].getAddress())).to.equal(100333333333);
-    expect(await stakedToken.balanceOf(recipient)).to.equal(200666666666);
-    expect(await stakedToken.totalSupply()).to.equal(toTokenAmount(3010));
+      const supplyIncrease = toTokenAmount(10);
+      await expect(stakedToken.distributeTokens(supplyIncrease))
+        .to.emit(stakedToken, 'LogTokenDistribution')
+        .withArgs(toTokenAmount(3010));
+
+      expect(await stakedToken.balanceOf(await _signers[0].getAddress())).to.equal(100333333333);
+      expect(await stakedToken.balanceOf(recipient)).to.equal(200666666666);
+      expect(await stakedToken.totalSupply()).to.equal(toTokenAmount(3010));
+    });
+
+
+    it("should not be callable by others", async function () {
+      const stakedTokenAsUser = this.stakedToken.connect(_signers[1]);
+      const amount = toTokenAmount(1);
+      await expect(stakedTokenAsUser.distributeTokens(amount))
+        .to.be.reverted;
+    });
   });
 
-  it("should update supply controller", async function () {
-    // const stakedToken: StakedToken = this.stakedToken
-    // await stakedToken.setSupplyController(await _signers[1].getAddress()).
+
+  describe("Allowances", async function () {
+    // Transfer + rebase ?
+
+    const owner = _signers[0];
+    const user = _signers[1];
+
+    it("should transfer if allowance is big enough", async function () {
+      const stakedToken: StakedToken = this.stakedToken
+      const stakedTokenAsUser = stakedToken.connect(user)
+      const allowance = toTokenAmount(1000)
+      const amount = toTokenAmount(10)
+
+      await expect(stakedToken.approve(await user.getAddress(), allowance))
+        .to.emit(stakedToken, 'Approval')
+        .withArgs(await owner.getAddress(), await user.getAddress(), allowance);
+
+      await expect(stakedTokenAsUser.transferFrom(await owner.getAddress(), await user.getAddress(), amount))
+        .to.emit(stakedTokenAsUser, 'Transfer')
+        .withArgs(await owner.getAddress(), await user.getAddress(), amount);
+
+      expect(await stakedToken.balanceOf(await owner.getAddress())).to.equal(this.initialSupply.sub(amount));
+      expect(await stakedToken.balanceOf(await user.getAddress())).to.equal(amount);
+      expect(await stakedToken.totalSupply()).to.equal(this.initialSupply);
+    });
+
+    it("should fail to transfer if the allowance is too small", async function () {
+      const stakedToken: StakedToken = this.stakedToken
+      const stakedTokenAsUser = stakedToken.connect(user)
+      const allowance = toTokenAmount(10)
+      const amount = toTokenAmount(100)
+
+      await expect(stakedToken.approve(await user.getAddress(), allowance))
+        .to.emit(stakedToken, 'Approval')
+        .withArgs(await owner.getAddress(), await user.getAddress(), allowance);
+
+      await expect(stakedTokenAsUser.transferFrom(await owner.getAddress(), await user.getAddress(), amount))
+        .to.be.reverted;
+    });
   });
 
-  // max supply?
-  // should set allowances
-  // should call dependent contracts on reward distribution
+
+  describe("External calls", async function () {
+
+    // should call dependent contracts on reward distribution
+  });
+
+
+
+
 }
