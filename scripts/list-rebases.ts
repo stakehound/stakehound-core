3c84174103db588fc8a248063ebd1b0a91eebe32
Edgars Nemše <edgars.nemse@gmail.com>
2020-12-15 19:25:16 +0200

    feat: enable listing all rebases for a token

---
+++ scripts/list-rebases.ts
@@ -0,0 +1,35 @@
+
+import { ethers } from "hardhat";
+import { Contract, ContractFactory } from "ethers";
+import { StakedToken } from "../typechain/StakedToken";
+
+async function main(): Promise<void> {
+  // Hardhat always runs the compile task when running scripts through it.
+  // If this runs in a standalone fashion you may want to call compile manually
+  // to make sure everything is compiled
+  // await run("compile");
+
+  const stakedTokenAddress = process.env.STAKED_TOKEN_ADDRESS || '';
+
+  const StakedToken: ContractFactory = await ethers.getContractFactory("StakedToken");
+  const stakedToken = StakedToken.attach(stakedTokenAddress) as StakedToken;
+
+  const filter = stakedToken.filters.LogTokenDistribution(null, null, null, null);
+  const rebases = await stakedToken.queryFilter(filter, 0);
+
+  console.log('block,timestamp,oldTotalSupply,newTotalSupply');
+
+  for(let r of rebases) {
+    const date =  new Date((await r.getBlock()).timestamp*1000);
+    console.log(`${r.blockNumber},"${date.toUTCString()}",${r.args?.oldTotalSupply},${r.args?.newTotalSupply}`)
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
